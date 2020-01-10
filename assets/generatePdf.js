e308d764b19a187ceb5e9b755c9b3a199c068397
TheFr0zen0ne <57323644+TheFr0zen0ne@users.noreply.github.com>
2020-01-10 11:06:23 +0100

    Update generatePdf.js

--- assets/generatePdf.js
+++ assets/generatePdf.js
@@ -1,4 +1,24 @@
+function genPDF(){
 var doc = new jsPDF()
+	//document.getElementsByTagName(td).innerHTML
+	
+	objectToArray(document.getElementsByTagName("td"));
+	
 
-doc.text('Hello world!', 10, 10)
-doc.save('a4.pdf')
+	doc.text(document.getElementsByTagName("td"), 10, 10);
+	doc.save('a4.pdf');
+}
+
+function objectToArray(obj){
+	arr = [];
+	
+	Object.keys(obj).forEach(function(key, index) {
+		arr[index] = obj.key;
+	});
+	
+	for (var prop in obj) {
+		if(Object.prototype.hasOwnProperty.call(obj, prop)) {
+			console.log(prop);
+		}
+	}
+}
